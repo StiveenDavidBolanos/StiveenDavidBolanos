0addc60b8e45bc2e7dd10d446049bd1b3cf60e73
StiveenBolaños <stiveenbolanost@gmail.com>
2024-09-11 18:14:35 -0500

    Creación de los Tiles completos

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,8 +8,47 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('HOla'),
+        title: const Text('HOla'),
       ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+        final appMenuItem = appMenuItems[index];
+        return _CustomListTile(
+          item: appMenuItem,
+        );
+      },
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({required this.item});
+  final MenuItems item;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      trailing: const Icon(
+        Icons.arrow_forward_ios_rounded,
+      ),
+      iconColor: colors.primary,
+      leading: Icon(item.icon),
+      title: Text(item.title),
+      subtitle: Text(item.subtitle),
+      onTap: () {},
     );
   }
 }
